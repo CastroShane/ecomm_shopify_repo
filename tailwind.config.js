94d24d5a12565ed1e39b053ae39f904fb1bce81d
Shane Castro <shaneh026@gmail.com>
2025-04-20 12:31:47 -0400

    product form init

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+MediaSourceHandle;
+module.exports = {
+  purge: [],
+  darkMode: false,
+  theme: {
+    extend: {
+      height: {
+        94: "22rem",
+      },
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+};
